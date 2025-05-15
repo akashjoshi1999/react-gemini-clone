566e302e9ebcd10921edeb148a09137587ce954b
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2025-05-15 14:00:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useEffect } from 'react';
 
 import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
-import { initGA } from './analytics';
+// import { initGA } from './analytics';
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-6V93120ZYM"); // Replace with your GA4 Measurement ID
+ReactGA.send("pageview");
 const App = () => {
-  useEffect(() => {
-    initGA();
-  }, []); // Corrected syntax
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  // useEffect(() => {
+  //   initGA();
+  // }, []); // Corrected syntax
 
   return (
     <>
@@ -18,3 +22,4 @@ const App = () => {
 };
 
 export default App;
+
